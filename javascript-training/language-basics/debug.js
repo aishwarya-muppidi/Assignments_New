3e85bb644c61163b74d0da761215ee763e9c1f4d
Aishwarya Muppidi <aishwarya.muppidi0603@gmail.com>
2026-04-09 23:24:05 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,19 @@
+console.log("Executing line 1");
+console.log("Executing line 2");
+console.log("Executing line 3");
+sumOfNumbers(5,10);
+console.log("Executing line 4");
+console.log("Executing line 5");
+console.log("Executing line 6");
+console.log("Executing line 7");
+console.log("Executing line 8");
+console.log("Executing line 9");
+console.log("Executing line 10");
+
+//logic to get sum of two numbers
+
+function sumOfNumbers(a,b)
+{
+    c=a-b;
+    console.log(c);
+}
---
+++ javascript-training/language-basics/variable-declaration.js
@@ -0,0 +1,13 @@
+var a;
+let b;
+const c=10;
+a=10;
+b=20;
+//c=30;
+a="Bharath";
+let b="Bharath";
+const c="Sharath"
+a="changing value"
+console.log(a);
+console.log(b);
+console.log(c);
