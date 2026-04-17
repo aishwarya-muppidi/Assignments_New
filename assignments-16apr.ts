a1aa099e55514f2d493384e136cdf48f74b8fa44
Gangadhar reddy <aishwarya.muppidi0603@gmail.com>
2026-04-17 15:47:29 +0530

    first commit

---
+++ assignments-16apr.ts
@@ -0,0 +1,88 @@
+//1.Temperature of a city in degrees Celsius: 25.5
+
+let temp:number=25.5;
+console.log(`1.Temparature of a city in degress Celsius: ${temp}`);
+
+//2.Whether a customer has placed an order: true or false
+
+let placeorder =true;
+let placeorder1 = placeorder ? "true" : "flase";
+console.log(`2.Whether a customer has placed an order: ${placeorder1}`);
+
+//3.Person's phone number: "123-456-7890"
+let person_phnum:string='123-456-7890';
+console.log(`3.Person's phone number: ${person_phnum}`);
+
+//4.Amount of money in a customer's bank account: 1000.50
+let amount:number=1000.50;
+console.log(`4.Amount of money in a customer's bank account:${amount}`);
+//output printed as 1000.5 its not printing 0 after 5
+
+//5.Person's email address: "john.doe@example.com"
+let person_email:string='john.doe@example.com';
+console.log(`5.Person's email address: ${person_email}`);
+
+//6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let latitude1:number=37.7749;
+let longitude1:number=-122.4194;
+console.log(`6.Coordinates of a location (latitude, longitude): ${latitude1}, ${longitude1}`);
+//7.person's marital status
+let isMarried = true;
+let maritalStatus = isMarried ? "Married" : "Single";
+console.log(`7.Marital Status: ${maritalStatus}`);
+
+//8.Person's occupation: "Software Engineer"
+let person_occupation:string='Software Engineer';
+console.log(`8.Person's occupation: ${person_occupation}`);
+
+//9.Person's favourite colour: "Blue"
+let person_favcolour:string='Blue';
+console.log(`9.Person's occupation: ${person_favcolour}`);
+
+//10.Current year: 2023
+let currentyear:number=2023;
+console.log(`10.Current year:${currentyear}`);
+
+//11.Number of followers on a social media platform: 1,000,000
+let noOfFollowers:string='1,000,000';
+console.log(`11.Number of followers on a social media platform: ${noOfFollowers}`);
+//string used to show commas in the 1,000,000
+
+//12.Rating of a movie: 7.5
+let ratingOfMovie:number=7.5;
+console.log(`12.Rating of a movie:${ratingOfMovie}`);
+
+//13.Person's blood type: 'A'
+let personBloodType='A';
+console.log(`13.Person's blood type: ${personBloodType}`);
+
+//14.Title of a book: "To Kill a Mockingbird"
+let titleOfBook:string='"To Kill a Mockingbird"'
+console.log('14.Title of a book:' + titleOfBook);
+
+//15.Number of employees in a company: 500
+let noOfEmployee:number=500;
+console.log(`15.Number of employees in a company:${noOfEmployee}`);
+
+//16.Time of an event: 2:30 PM
+let timeOfEvent:String='2:30 PM';
+console.log(`16.Time of an event: ${timeOfEvent}`);
+
+//17.Name of a country: "United States"
+let nameOfCountry='United States';
+console.log(`17.Name of a country: ${nameOfCountry}`);
+
+//18.Person's eye color: "Brown"
+//let personEyeColur:string='"Brown"';
+//console.log('18.Person's eye color:'+ personEyeColur);
+
+
+//19.Person's birthplace: "New York City"
+//let personBirthPlace='"New York City"';
+//console.log('19.Person's birthplace: '+personBirthPlace);
+
+
+//20.Distance between two cities: 200.5
+let distanceBtwCities:number=200.5;
+console.log(`20.Distance between two cities:${distanceBtwCities}`);
+
